a12986f11166815f868065b3c26b815893ac4683
Philipp Kyeck <philipp@beta-interactive.de>
2014-10-16 17:02:55 +0200

    add custom stuff: - template loader - loader logic - filters (static right now) - tags (static right now)

--- index.js
+++ index.js
@@ -6,13 +6,58 @@ var fs = require('fs');
 var nunjucks = require('nunjucks');
 
 
+function MyLoader(opts) {
+  this.modulesPath = opts.modules;
+  this.ext = opts.ext;
+}
+
+MyLoader.prototype.getSource = function(name) {
+  console.log('MyLoader getSource()', name);
+
+  var splits = name.split('/');
+
+  if (splits[0] === '@') {
+    splits.shift();
+    name = this.modulesPath + '/' + splits.join('/') + this.ext;
+  }
+
+  if (name.lastIndexOf('.') < 0) {
+    name += this.ext;
+  }
+
+  return {
+    src: fs.readFileSync(name).toString(),
+    path: name,
+    noCache: true
+  };
+}
+
+
 module.exports = function(opt) {
   opt = opt || {};
 
   var ext = opt.ext || '.html';
-  var config = opt.ejs || {};
+  var context = opt.context || {};
   var baseDir = opt.baseDir || __dirname;
   var bsURL = '';
+  var modules = opt.modulesDir || '';
+
+  var filters = {
+    filter: function(input, condition) {
+      return input.filter(function(item) {
+        return eval(condition);
+      });
+    },
+    min: function(input, other) {
+      return Math.min(input, other);
+    },
+    range: function(input, start, end) {
+      return input.slice(start, end);
+    },
+    parseIntOr: function(input, or) {
+      return parseInt(input, 10) || or;
+    }
+  };
   
   if (opt.browserSync) {
     if (opt.browserSync === true) {
@@ -28,14 +73,32 @@ module.exports = function(opt) {
     var pathname = path.join(baseDir, url.parse(file).pathname);
 
     if (path.extname(pathname) === ext && fs.existsSync(pathname)) {
-      // var contents = fs.readFileSync(pathname).toString();
+      context.query = url.parse(req.url, true).query;
+      context.filename = pathname;
 
-      config.query = url.parse(req.url, true).query;
-      config.filename = pathname;
+      var env = new nunjucks.Environment(new MyLoader({
+        modules: modules,
+        ext: ext
+      }), {
+        watch: false,
+        tags: {
+          blockStart: '<%',
+          blockEnd: '%>',
+          commentStart: '<#',
+          commentEnd: '#>'
+        }
+      });
 
-      nunjucks.render(pathname, config, function(err, result) {
+      for (var filterName in filters) {
+        console.log(filterName);
+        env.addFilter(filterName, filters[filterName]);
+      }
+
+      console.log('nunjucks render:', pathname);
+      env.render(pathname, context, function(err, result) {
         if (err) {
-          console.log(err, err.stack);
+          console.log(err);
+          console.log(err.stack);
           res.writeHead(500);
           res.end();
           return;
